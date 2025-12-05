e5e7d6641a7416082210066ecbcd20b2d7dc502d
jeffjeff4-0 <chatandtest1@gmail.com>
2025-12-04 20:52:24 -0800

    add matrixAdditionV0.cu

---
+++ leetGpu/matrixAdditionV0.cu
@@ -0,0 +1,20 @@
+#include <cuda_runtime.h>
+
+__global__ void matrix_add(const float* A, const float* B, float* C, int N) {
+    const auto tid = threadIdx.x;
+    const auto id = blockDim.x * blockIdx.x + tid;
+
+    if (id >= N) {
+        return;
+    }
+    C[id] = A[id] + B[id];
+}
+
+// A, B, C are device pointers (i.e. pointers to memory on the GPU)
+extern "C" void solve(const float* A, const float* B, float* C, int N) {
+    int threadsPerBlock = 256;
+    int blocksPerGrid = (N * N + threadsPerBlock - 1) / threadsPerBlock;
+
+    matrix_add<<<blocksPerGrid, threadsPerBlock>>>(A, B, C, N*N);
+    // cudaDeviceSynchronize();
+}
